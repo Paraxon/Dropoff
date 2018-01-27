7de9dc793107e63bbc8c1eff83219dab1155704a
James Metke <james_metke@hotmail.com>
2018-01-27 14:49:10 -0700

    Much progress very wow

---
+++ Assets/Scripts/Contact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum ContactState
+{
+    Arriving,
+    Moving,
+    Waiting
+}
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Contact : MonoBehaviour {
+
+    public ContactState currentState;
+    public Vector3 dropoffPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (currentState)
+        {
+            case ContactState.Moving:
+                break;
+            case ContactState.Waiting:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
