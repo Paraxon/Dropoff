26cf48eb8edae9d996df30223679d4a58b670daf
ryanbromsgrove <ryan.bromsgrove@gmail.com>
2018-01-28 14:07:59 -0700

    More audio

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,74 +4,73 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
 
+
+
 	public AudioSource[] ThreatSources = new AudioSource[4];
+	public PlayerController Player;
 	//TODO Probably some kinda time delta time would be better...
-	public float increment = 0.05f;
+	public float VolumeIncrement = 0.01f;
 	//Corresponding to above sources, 0 means no change, -1 means decreasing, 1 means increasing
 	private int[] ThreatState = new int[4] { 0, 0, 0, 0 };
 
-	public int CurrentThreat = 0;
+	public float CurrentThreat = 0;
+	public float AutoIncrement = 0;
+	public bool Testing = false;
 
 	public void ChangeVolume () {
 		int i = 0;
 		while (i < ThreatState.Length) {
 			if (ThreatSources [i].volume != 1 && ThreatState [i] == 1) {
 				//Increment up
-				ThreatSources[i].volume += increment;
+				ThreatSources[i].volume += VolumeIncrement;
 			} else if (ThreatSources [i].volume != 0 && ThreatState [i] == -1) {
 				//Increment down
-				ThreatSources[i].volume -= increment;
+				ThreatSources[i].volume -= VolumeIncrement;
 			} else {
 				//No need to change
 				ThreatState[i] = 0;
 			}
-			i++;
 		}
 	}
 
-	public void CheckThreat(int Threat) {
+	public void CheckThreat(float Threat) {
 		//This should allow for threat level changes
 		//Transition points are 25%, 50%, 75%, 100%
 
 		if (CurrentThreat < 25 && Threat >= 25) {
 			//Increasing from 0 to 1
+			Debug.Log("Threat increasing to low");
 			ThreatState [0] = 1;
 		}
 		if (CurrentThreat < 50 && Threat >= 50) {
 			//Increasing from 1 to 2
+			Debug.Log("Threat increasing to mid");
+
 			ThreatState[1] = 1;
 		}
 		if (CurrentThreat < 75 && Threat >= 75) {
 			//Increasing from 2 to 3
+			Debug.Log("Threat increasing to high");
+
 			ThreatState[2] = 1;
 		}
 		if (CurrentThreat < 100 && Threat >= 100) {
 			//Increasing from 3 to 4
+			Debug.Log("Threat increasing to fucked");
+
 			ThreatState[3] = 1;
 		}
 		if (CurrentThreat >= 100 && Threat < 100) {
-			//Decrease from 4 to 3 (shouldn't happen)
-			ThreatState[3] = -1;
-		}
-		if (CurrentThreat >= 75 && Threat < 75) {
-			//Decrease from 3 to 2
-			ThreatState[2] = -1;
-		}
-		if (CurrentThreat >= 50 && Threat < 50) {
-			//Decrease from 2 to 1
-			ThreatState[1] = -1;
-		}
-		if (CurrentThreat >= 25 && Threat < 25) {
-			//Decrease from 1 to 0
-			ThreatState[0] = -1;
+				ThreatState[0] = -1;
 		}
 		CurrentThreat = Threat;
 	}
 
+
 	// Use this for initialization
 	void Start () {
 		//As we start, all tracks are silent
-
+		Player = GameObject.Find("Player").GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
@@ -80,7 +79,16 @@ public class MusicPlayer : MonoBehaviour {
 		ThreatSources[1].timeSamples = ThreatSources[0].timeSamples;
 		ThreatSources[2].timeSamples = ThreatSources[0].timeSamples;
 		ThreatSources[3].timeSamples = ThreatSources[0].timeSamples;
-		CheckThreat (GameObject.Find ("Player").GetComponent<PlayerController> ().currentHeat);
+		if (Testing) {
+			if (CurrentThreat > 120 || CurrentThreat < 0) {
+				AutoIncrement = -AutoIncrement;
+			}
+			float NewThreat = CurrentThreat + AutoIncrement;
+			CheckThreat (NewThreat);
+		}
+		if (Player.maxHeat != 0) {
+			CheckThreat (Player.currentHeat / Player.maxHeat * 100f);
+		}
 		ChangeVolume ();
 	}
 }
